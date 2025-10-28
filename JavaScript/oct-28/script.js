0d72185c6d69cb1a668fc1c9503de881c64f5a95
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2025-10-28 15:39:27 +0530

    October 29
    JavaScript Operators and type conversion.

---
+++ JavaScript/oct-28/script.js
@@ -0,0 +1,59 @@
+// let a = prompt("Enter a number:", 0);
+// let b = prompt("Enter another number:", 0);
+
+// console.log("Datatype of a:", typeof a);
+// console.log("Datatype of b:", typeof b);
+// let sum1 = a + b;
+
+// console.log("Sum1:", sum1);
+// console.log("Datatype of sum1:", typeof sum1);
+
+// let sum2 = Number(a) + Number(b);
+/* Can also be written as */
+// let sum2 = +a + +b;
+// console.log("Sum2:", sum2);
+// console.log("Datatype of sum2:", typeof sum2);
+
+/* Note: While performing any opration using operators their datatype can change which is also known as Typecasting.
+Or we can say change of a value from one datatype to another is called typecasting.
+
+There are two type of typecasting.
+1. Implicit typecasting: when datatype changes automatically.
+2. Explicit typecasting: when we force it to change datatype using functions or any other method.
+*/
+
+/* ======== Above code could also be written as to work correctly ======== */
+// let a = +prompt("Enter a number:", 0);
+// let b = +prompt("Enter another number:", 0);
+
+// console.log("Datatype of a:", typeof a);
+// console.log("Datatype of b:", typeof b);
+// let sum1 = a + b;
+
+// console.log("Sum1:", sum1);
+// console.log("Datatype of sum1:", typeof sum1);
+
+/* ======================================================================= */
+// let a = prompt("Enter a number:", 0);
+// let b = prompt("Enter another number:", 0);
+
+// console.log("a + b =", a + b);
+// console.log("a + b =", Number(a) + Number(b));
+// console.log("a + b =", +a + +b);
+// console.log("a - b =", a - b);
+// console.log("a / b =", a / b);
+// console.log("a * b =", a * b);
+// console.log("a ** b =", a ** b);
+// console.log("a % b =", a % b);
+
+/* 
+Built-in functions covered today:
+1. Number()
+2. String()
+3. Boolean()
+4. parseInt()
+5. parseFloat()
+6. alert()
+7. prompt()
+
+*/
