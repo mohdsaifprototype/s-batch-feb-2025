ad5e02e982c7d92ebe23d11236c1116030e341c8
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2025-11-11 15:39:30 +0530

    November 11
    JavaScript Events and Escape character sequence!

---
+++ JavaScript/nov-11/script.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+console.log('Ram\'s brother\t said that:\n \"The run rises from the east and sets in the west\"');
+
+function event1() {
+  document.body.style.backgroundColor = "#cba";
+  alert("Don't click this para again!");
+}
